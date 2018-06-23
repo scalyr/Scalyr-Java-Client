0a01c0a7bf03610e05accf67fda43b2adc5d61bb
Oliver Hsu <oliver@scalyr.com>
2018-06-22 17:55:08 -0700

    PLATFORM-4 Update Scalyr-Java-Client

--- src/com/scalyr/api/knobs/util/package-info.java
+++ src/com/scalyr/api/knobs/util/package-info.java
@@ -1,4 +1,7 @@
 /**
- * This package defines utilities that add value to the Scalyr Knobs API.
+ * This package contains the API for the Scalyr system, aka Knobs. To get started with the API,
+ * see <a href="https://www.scalyr.com/help/java-api">https://www.scalyr.com/help/java-api</a>.
+ * To learn more about Scalyr configuration, see
+ * <a href="https://www.scalyr.com/help/config">https://www.scalyr.com/help/config</a>.
  */
-package com.scalyr.api.knobs.util;
+package com.scalyr.api.knobs;
