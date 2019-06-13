81ccf56fd0a7793606c37c152d4950a78380d249
Vaibhav Bhat <scalyr-loaner12@Scalyr-Loaner12s-MacBook-Pro.local>
2019-06-12 17:33:51 -0700

    Moved @Ignore in test

--- src/test/com/scalyr/api/tests/GzipTest.java
+++ src/test/com/scalyr/api/tests/GzipTest.java
@@ -19,27 +19,23 @@ import static org.junit.Assert.assertFalse;
  * or a remote Scalyr environment. You must manually check that the test logs are received and decompressed
  * correctly at the destination.
  */
-public class GzipTest extends LogsTestBase {
-  @Test @Ignore
-  public void testGzipOnJavaNetHttpClient() {
+@Ignore public class GzipTest extends LogsTestBase {
+
+  @Test public void testGzipOnJavaNetHttpClient() {
     // Put appropriate log write token into LogService()
     LogService testService = new LogService("0rXw/66WWhDPKouk0rZDkePU7D6bWpoQTOGYFxH/K1yc-");
     // Either put localhost here, or a particular Scalyr environment.
     testService = testService.setServerAddress("http://localhost:8080");
 
     Events._reset("testGzipSession", testService, 999999, false, true);
-
     // Enable Gzip if not enabled by default
     if (!Events.ENABLE_GZIP_BY_DEFAULT) Events.enableGzip();
 
     Events.info(new EventAttributes("tag", "testWithGzipJava", "foo1", "bla1", "foo2", "bla2"));
-
     Events.flush();
-
   }
 
-  @Test @Ignore
-  public void testGzipOnApacheHttpClient() {
+  @Test public void testGzipOnApacheHttpClient() {
     Knob.setDefaultFiles(new ConfigurationFile[0]);
 
     // Put appropriate log write token into LogService()
@@ -48,7 +44,6 @@ public class GzipTest extends LogsTestBase {
     testService = testService.setServerAddress("http://localhost:8080");
 
     Events._reset("testGzipSession", testService, 999999, false, true);
-
     // Enable Gzip if not enabled by default
     if (!Events.ENABLE_GZIP_BY_DEFAULT) Events.enableGzip();
 
@@ -56,10 +51,8 @@ public class GzipTest extends LogsTestBase {
     TuningConstants.useApacheHttpClientForEventUploader = new Knob.Boolean("foo", true);
 
     Events.info(new EventAttributes("tag", "testWithGzipApache", "foo1", "bla1", "foo2", "bla2"));
-
     Events.flush();
 
     TuningConstants.useApacheHttpClientForEventUploader = null; // Set it back to original value of null
-
   }
 }
