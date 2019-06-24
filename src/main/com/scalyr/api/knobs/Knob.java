517cf228248ed241bcef9d76bbe970227ec067b1
Vaibhav Bhat <scalyr-loaner12@Scalyr-Loaner12s-MacBook-Pro.local>
2019-06-24 16:35:09 -0700

    fixed NPE

--- src/main/com/scalyr/api/knobs/Knob.java
+++ src/main/com/scalyr/api/knobs/Knob.java
@@ -526,7 +526,9 @@ public class Knob {
     }
 
     @Override public java.time.Duration getWithTimeout(java.lang.Long timeoutInMs, boolean bypassCache) throws ScalyrDeadlineException {
-      return java.time.Duration.of((long) super.getWithTimeout(timeoutInMs, bypassCache), ChronoUnit.NANOS);
+      java.lang.Long value = (long) super.getWithTimeout(timeoutInMs, bypassCache);
+      if (value == null) return null;
+      return java.time.Duration.of(value, ChronoUnit.NANOS);
     }
 
     @Override public Duration expireHint(java.lang.String dateStr) {
